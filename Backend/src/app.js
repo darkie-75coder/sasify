de9354f8fcb52dd9ea40416d5db44609060a67d3
darkie-75coder <shivanshkumargupta74@gmail.com>
2026-03-27 18:36:13 +0530

    add origin to local also

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://sasifer.vercel.app", "https://sasify.onrender.com"],
+    origin: [
+      "http://localhost:5173",
+      "https://sasifer.vercel.app",
+      "https://sasify.onrender.com",
+    ],
     credentials: true,
   }),
 );
